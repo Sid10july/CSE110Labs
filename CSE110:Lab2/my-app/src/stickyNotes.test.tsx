ecc702ee3ce3138a27cda42d9c05e58b825aeb75
Kiruthika Marikumaran <kiruthika.star14@gmail.com>
2024-10-19 17:15:50 -0700

    error

--- CSE110:Lab2/my-app/src/stickyNotes.test.tsx
+++ CSE110:Lab2/my-app/src/stickyNotes.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react"
 import { StickyNotes } from "./stickyNotes";
 
 describe("Create StickyNote", () => {
